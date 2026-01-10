087fc1e262ae676d74e719d2cf85e493474c247e
janelldizon4-debug <janelldizon4@gmail.com>
2026-01-10 20:39:19 +0800

    Update key.js

--- key.js
+++ key.js
@@ -1,4 +1,4 @@
 const REGISTERED_KEYS = [
-{ accessKey: "Cris-rank-2025", name: "CrisUser1", subscription: "1 Month", revoked:false, expires:"2026-02-28" },
+{ accessKey: "Cris-rank-2025", name: "CrisUser1", subscription: "1 Month", revoked:false, expires:"2026-01-10" },
 { accessKey: "Cris-rank-2026", name: "CrisUser2", subscription: "Infinite", revoked:false, expires:"2099-12-31" }
 ];
