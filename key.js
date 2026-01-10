4b9181c12fbc703dcb74b694d495d1c49adfd677
janelldizon4-debug <janelldizon4@gmail.com>
2026-01-10 19:12:33 +0800

    Update key.js

--- key.js
+++ key.js
@@ -1,1 +1,21 @@
-key
+
+const REGISTERED_KEYS = [
+  {
+    name: "Cris",
+    accessKey: "CRIS-2026-KING",
+    expires: "2026-12-31",
+    revoked: false
+  },
+  {
+    name: "Janell",
+    accessKey: "JANELL-EXPIRED",
+    expires: "2025-01-01",
+    revoked: false
+  },
+  {
+    name: "BannedUser",
+    accessKey: "BANNED-KEY",
+    expires: "2026-05-01",
+    revoked: true
+  }
+];
